4e64c54805d026a35e775c0b365a3613447eeb4e
VickyShinny <victoriaoluwaferanmi@gmail.com>
2023-01-27 17:51:13 +0100

    project

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -6,7 +6,7 @@ export const projectData = [
     name: "Ecommerce Application",
     image: Handout,
     github: "HandoutProject",
-    live: "",
+    live: "https://stutern-handout.web.app/",
     about:
       "This platform allows people to give out useful but unwanted items for free or a give away price to others who may need them.",
   },
